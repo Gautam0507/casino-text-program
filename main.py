013ca392d96e13980d1695f40a364b720e8900dd
Gautam Laptop <gautam.nraj@gmail.com>
2022-11-01 00:05:01 +0530

    get_number_of_lines function has been completed

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 
 from curses.ascii import isdigit
+import imp
 
+
+MAX_LINES = 5
 
 def deposit():
     while True:
-        amount = input("What would you like to deposit? $")
+        amount = input("What would you like to deposit?")
         if amount.isdigit():
             amount = int(amount)
             if amount > 0:
@@ -14,3 +17,23 @@ def deposit():
         else: 
             print("Please enter a number.")
     return amount
+
+def get_number_of_lines():
+    while True: 
+        lines = input("Enter the number of lines to bet on (1" + str(MAX_LINES)+ ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("The number must be between 1 and " + str(MAX_LINES))
+        else: 
+            print("Please enter a number")
+    return lines
+
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    
+main()
